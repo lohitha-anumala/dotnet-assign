212bb9ba1054993b0cc7ce8a776c9e72ffaee420
lohitha-anumala <68274764+lohitha-anumala@users.noreply.github.com>
2020-08-13 17:49:51 +0530

    Add files via upload

---
+++ ClassCalcLib/Operators.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassCalcLib
+{
+    [Serializable]
+    public class Operators
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+        public int Add()
+        {
+            return num1 + num2;
+        }
+        public int Subtract()
+        {
+            return num1 - num2;
+        }
+        public int Multiply()
+        {
+            return num1 * num2;
+        }
+        public float Divide()
+        {
+            return num1 / num2;
+        }
+        public int Modulus()
+        {
+            return num1 % num2;
+        }
+    }
+}
+    
